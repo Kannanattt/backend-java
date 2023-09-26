14182cfa887528c1310a60fb90e34b546696d066
Kannanatt <6310210022@psu.ac.th>
2023-09-26 22:16:09 +0700

    lab11

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.example.demo.controller;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.StreamingHttpOutputMessage.Body;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +23,9 @@ import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.SkillRepository;
 
 @RestController
+
+@CrossOrigin(origins = "*")
+
 public class EmployeeController {
 	
 	@Autowired
